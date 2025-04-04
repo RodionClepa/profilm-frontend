f5161fcbc7fca4d647256189a31479886c40361f
RodionClepa <raduclepa@gmail.com>
2025-04-04 08:46:22 +0300

    fix(layout): remove unnecessary file and code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-enum Mode {
-  LIGHT = 'light',
-  DARK = 'dark',
-}
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
