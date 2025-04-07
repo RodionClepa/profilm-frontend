62d096f4c1561bc4ded18a6c78889889170da471
RodionClepa <raduclepa@gmail.com>
2025-04-07 10:51:34 +0300

    feat(landing): add api service

---
+++ src/app/shared/services/api/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() { }
+
+  private api(): string {
+    return environment.apiUrl + '/api';
+  }
+
+  private movies(): string {
+    return `${this.api()}/movies`
+  }
+
+  public popularMovies(): string {
+    return `${this.movies()}/popular?page=501`;
+  }
+}
