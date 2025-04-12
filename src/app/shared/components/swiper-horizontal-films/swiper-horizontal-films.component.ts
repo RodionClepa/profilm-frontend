b09c0ca665528c58eff7c320d9a9d774166f0c9e
RodionClepa <raduclepa@gmail.com>
2025-04-12 12:41:48 +0300

    feat(landing): add skeleton for promo and gradient for landing sections

--- src/app/shared/components/swiper-horizontal-films/swiper-horizontal-films.component.ts
+++ src/app/shared/components/swiper-horizontal-films/swiper-horizontal-films.component.ts
@@ -24,9 +24,9 @@ export class SwiperHorizontalFilmsComponent {
   skeletonCards = [1, 2, 3, 4, 5, 6];
 
   breakpoints = {
-    320: { slidesPerView: 2, spaceBetween: 10, slidesPerGroup: 2 },
-    480: { slidesPerView: 3, spaceBetween: 20, slidesPerGroup: 3 },
-    768: { slidesPerView: 4, spaceBetween: 20, slidesPerGroup: 4 },
-    1024: { slidesPerView: 6, spaceBetween: 20, slidesPerGroup: 6 },
+    320: { slidesPerView: 2, spaceBetween: 12, slidesPerGroup: 2 },
+    480: { slidesPerView: 3, spaceBetween: 16, slidesPerGroup: 3 },
+    768: { slidesPerView: 4, spaceBetween: 16, slidesPerGroup: 4 },
+    1024: { slidesPerView: 6, spaceBetween: 16, slidesPerGroup: 6 },
   };
 }
