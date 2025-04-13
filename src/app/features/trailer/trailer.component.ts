11f4894f33b103dc81b6d2721a4d28a8c2bfac18
RodionClepa <raduclepa@gmail.com>
2025-04-13 18:10:33 +0300

    fix(movie-details): fix trailer computed error

--- src/app/features/trailer/trailer.component.ts
+++ src/app/features/trailer/trailer.component.ts
@@ -1,26 +1,31 @@
 import { Component, computed, input } from '@angular/core';
 import { MovieVideo } from '../../shared/types/movie-details.type';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+
+interface TrailerVideo extends MovieVideo {
+  youtubeUrl: SafeResourceUrl
+}
 
 @Component({
   selector: 'app-trailer',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './trailer.component.html',
-  styleUrl: './trailer.component.scss'
+  styleUrl: './trailer.component.scss',
 })
 export class TrailerComponent {
   videos = input<MovieVideo[]>([]);
-  formattedVideos = computed(() => {
-    return this.videos().map((video) => ({
-      ...video,
-      link: this.getSafeUrl(video.link)
-    }))
-  })
-
+  formattedVideos: TrailerVideo[] = [];
   constructor(private sanitizer: DomSanitizer) { }
 
+  ngOnInit() {
+    this.formattedVideos = this.videos().map((video) => ({
+      ...video,
+      youtubeUrl: this.getSafeUrl(video.link)
+    }))
+  }
+
   private getSafeUrl(link: string): SafeResourceUrl {
+    console.log("getSafeUrl")
     const embedUrl = `https://www.youtube.com/embed/${link}`;
     return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
   }
